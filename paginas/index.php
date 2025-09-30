5d911e5153ff0d54d704e7f3b1d0b67dc15783b8
luis <luisfelipedemattos01@gmail.com.br>
2025-09-30 17:31:07 -0300

    Atualizações no index

---
+++ paginas/index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Shift Node</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="estilo/inicial.css">
+
+</head>
+<body>
+
+    <header>
+        <nav class="navbar">
+            <div class="logo">Shift Node</div>
+
+            <ul>
+                <li><a href="#">Agendar</a></li>
+                <li><a href="#">Serviços</a></li>
+                <li><a href="#">Quem Somos</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+
+            <div class="profile">
+                <span>Cliente</span>
+                <i class="fa-solid fa-user"></i>
+            </div>
+
+            <div class="hamburger" onclick="toggleMenu()">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </nav>
+    </header>
+
+<main>
+
+  <!-- Fundo com imagem -->
+  <section class="fundo">
+    <div class="fundo-content">
+      <h2>Por que escolher nosso serviço?</h2>
+      <ul>
+        <li>Alto Padrão</li>
+        <li>Equipe Qualificada</li>
+        <li>Diagnóstico Preciso</li>
+        <li>Ferramental Específico</li>
+        <li>Performance e Segurança</li>
+      </ul>
+    </div>
+  </section>
+
+  <!-- Quem Somos -->
+  <section class="quem-somos">
+    <h3>Especialistas em manutenções gerais, e cuidados com seu Automóvel</h3>
+
+    <br>
+
+    <h1 class="titulo">Quem Somos</h1>
+
+    <br>
+
+    <p>
+A 4 Nitros – É uma oficina especializada em manutenção preventiva e corretiva básica, com foco na segurança e desempenho dos veículos.
+A empresa se destaca pelo atendimento ágil, transparente e confiável, fornecendo orçamentos claros e orientações técnicas que permitem ao cliente entender a real condição do seu carro.
+    </p>
+
+
+    <div class="galeria">
+    <img src="img_site/pastilha.jpg" alt="Oficina 1">
+    <img src="img_site/óleomotor.jpg" alt="Oficina 2">
+    <img src="img_site/fluidoveiculo.jpg" alt="Oficina 3">
+    <img src="img_site/arrefecimento.jpg" alt="Oficina 4">
+    </div>
+
+  </section>
+
+</main>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+</html>
+
